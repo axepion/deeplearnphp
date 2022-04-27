4605cf95fddc697f96ff9614756a97c4ffc4f05f
axepion <axepion7250@gmail.com>
2022-04-27 15:24:05 +0300

    init project

---
+++ backend/base/settings/internal_settings.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('VG_ACCESS') or die("Access denied");
+
+const TEMPLATES = 'templates/default';
+const ADMIN_TEMPLATES = 'backend/admin/views';
+
+const COOKIE_VERSION = '1.0.0';
+const CRYPT_KEY = '';
+const COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const OTY = 8;
+const OTY_LINKS = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts' => [],
+];
+
+const USER_CSS_JS = [
+    'styles' => [],
+    'scripts' => [],
+];
---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('VG_ACCESS') or die("Access denied");
+
+const SITE_URL = 'http://deeplearnphp';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'deeplearnphp_db';
+
+
+echo 'hello';
